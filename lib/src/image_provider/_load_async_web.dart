9d66f46511d27193b58cee11a267b3fe34ff02b4
Luigi Rosso <luigi.rosso@gmail.com>
2020-10-20 13:33:38 -0700

    Zone to catch ProgressEvent.

--- lib/src/image_provider/_load_async_web.dart
+++ lib/src/image_provider/_load_async_web.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:html';
 import 'dart:ui' as ui;
 
 import 'package:flutter/rendering.dart';
@@ -9,18 +10,29 @@ Future<ui.Codec> loadAsyncHtmlImage(
   CachedNetworkImageProvider key,
   StreamController<ImageChunkEvent> chunkEvents,
   DecoderCallback decode,
-) {
+) async {
   final resolved = Uri.base.resolve(key.url);
 
-  return ui.webOnlyInstantiateImageCodecFromUrl( // ignore: undefined_function
-    resolved,
-    chunkCallback: (int bytes, int total) {
-      chunkEvents.add(
-        ImageChunkEvent(
-          cumulativeBytesLoaded: bytes,
-          expectedTotalBytes: total,
-        ),
-      );
-    },
-  ) as Future<ui.Codec>; // ignore: undefined_function
+  return runZonedGuarded<Future<ui.Codec>>(() async {
+    return await (ui.webOnlyInstantiateImageCodecFromUrl(
+      // ignore: undefined_function
+      resolved,
+      chunkCallback: (int bytes, int total) {
+        chunkEvents.add(
+          ImageChunkEvent(
+            cumulativeBytesLoaded: bytes,
+            expectedTotalBytes: total,
+          ),
+        );
+      },
+    ) as Future<ui.Codec>);
+  }, (error, __) {
+    if (error is ProgressEvent) {
+      // Intentionally empty, swallowing ProgressEvent (Flutter Web seems to
+      // throw this ProgressEvent when doing certain operations on an HTTP
+      // request). See https://github.com/flutter/flutter/issues/60239
+    } else {
+      throw error;
+    }
+  });
 }
